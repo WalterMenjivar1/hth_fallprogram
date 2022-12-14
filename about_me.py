aef0b4c75f3cf33f9df45dd95afcb455ae86927e
Walter <waltermenjivar431@gmail.com>
2022-12-13 18:28:45 -0800

    Add about_me.py

---
+++ about_me.py
@@ -0,0 +1,13 @@
+full_name ="Walter Menjivar"
+Preferred_name= "Walter"
+Age = 18
+has_used_python = True
+
+hobbies={"soccer","watching tv shows","taking walks","biking"}
+print(Hobbies)
+
+fav_things = {"movie": "Avengers infinity war",
+"tv shows": "the flash","food": "pizza",
+"hobbie" : "playing soccer","number" : 8}
+
+all_vars = dict(vars())
