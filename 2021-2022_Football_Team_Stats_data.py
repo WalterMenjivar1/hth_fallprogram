eb8f35908cc57d11b94c81acdf63bc543dd15bae
Walter <waltermenjivar431@gmail.com>
2022-12-13 18:32:31 -0800

    Add Finding an interesting dataset

---
+++ 2021-2022_Football_Team_Stats_data.py
@@ -0,0 +1,18 @@
+import pandas
+
+pandas.options.display.max_columns = None
+pandas.options.display.max_rows = None
+
+2021_2022_Football_Team_Stats_data = pandas.read_csv("{players_20.csv}")
+
+10 questions:
+1)Does being from a surtain country play a role in how successful a team is?
+2)How much can a coach with a lot of experience play a role in a team's winning strategy?
+3)Does a players height correlate to the amount of goals they score? 
+4)Does a player's experience matter in how much it contributes to the team?
+5)Does a scoring a lot of goals mean a team is eligible to win the cup? 
+6)Is there a correlation between the amount you practice and the amount of goals you will make? 
+7)Does the conditions a player grow up in affect the way they play? 
+8)It is more important to score goals or do assists?
+9)Does a goalkeer's height effect the amount of goals they will be scored on?
+10)Does getting draws mean a team is bad?
